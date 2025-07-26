dfdf02d099316dfb13a51ddcca9020773a97a405
desxtra <malvinsans@gmail.com>
2025-07-27 01:16:39 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { serve } from '@hono/node-server'
+import nodemailer from 'nodemailer'
+import 'dotenv/config'
+
+const app = new Hono()
+
+app.use('*', cors())
+
+app.get('/', (c) => c.text('Hello from Hono.js!'))
+
+app.post('/api/contact', async (c) => {
+  const body = await c.req.parseBody()
+  const { name, email, message } = body
+
+  console.log('EMAIL_USER:', process.env.EMAIL_USER)
+  console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? ' Loaded' : 'Missing')
+  console.log('EMAIL_TO:', process.env.EMAIL_TO)
+
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    }
+  })
+
+  try {
+    await transporter.sendMail({
+      from: `"${name}" <${email}>`,
+      to: process.env.EMAIL_TO,
+      subject: 'New message from portfolio',
+      text: message,
+    })
+
+    console.log('Email sent from:', name)
+    return c.redirect('http://localhost:4321/thankyou')
+
+  } catch (err) {
+    console.error('Error sending email:', err)
+    return c.json({ status: 'error', message: 'Failed to send email' }, 500)
+  }
+})
+
+serve({
+  fetch: app.fetch,
+  port: 8787
+})
